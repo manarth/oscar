b4760fe992fb6dea0dbf2840907a84405d228a94
Marcus Deglos <marcus@deglos.com>
2012-05-24 17:02:00 +0200

    Improved packaging documentation.

--- package.sh
+++ package.sh
@@ -2,15 +2,16 @@
 # This is the script that's being used to package the .box.
 
 # Base box.
-base="ProjectOscar_1337284846"
+# This is the VBox identity (found through `VBoxManage list runningvms`).
+base="ProjectOscar_1337384725"
 
-# Path to the directory of this script.
+# Automatically get the path to the directory of this script.
 cwd="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Path to the .box file.
+# Path to the .box file will be a subdir called "packages".
 filepath="$cwd/packages"
 
-# Filename for the .box file
+# Filename for the .box file.
 filename="project_oscar.box"
 
 boxfile="$filepath/$filename"
@@ -26,3 +27,4 @@ echo "Packaging $boxfile."
 
 # Package that box.
 vagrant package --base $base --output $boxfile --vagrantfile Vagrantfile --include puppet/
+
